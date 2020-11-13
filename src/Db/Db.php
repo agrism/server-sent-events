4a4ff701d82920c5c65a46ea0f8af90a55df7f5a
Agris <7924@inbox.lv>
2020-11-13 02:12:31 +0200

    remove db connection messahe

--- src/Db/Db.php
+++ src/Db/Db.php
@@ -51,7 +51,6 @@ class Db
 			$this->connection = new PDO("mysql:host=$this->host;dbname=$this->dbName", $this->username, $this->password);
 			// set the PDO error mode to exception
 			$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			echo "Connected successfully";
 		} catch(PDOException $e) {
 			echo "Connection failed: " . $e->getMessage();
 		}
